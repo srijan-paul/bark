1a1efb762f7b6fb968638aa5cf552863e2e3c31e
injuly <srijannr1@gmail.com>
2021-12-17 22:54:19 +0530

    simple splash info added

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,12 @@ module Main where
 
 import Bark.Core (buildProject)
 import Control.Concurrent (threadDelay)
-import Control.Monad (forever)
+import Control.Monad (forever, when)
 import System.Directory (createDirectoryIfMissing)
 import System.FSNotify (watchDir, withManager)
 import System.FilePath ((</>))
+import System.Environment (getArgs)
+import System.Exit (exitSuccess)
 
 
 buildAndLog :: FilePath -> IO ()
@@ -20,6 +22,20 @@ watchProject dir =
         watchDir mgr contentPath (const True) (\_ -> buildAndLog dir)
         forever $ threadDelay 1000000
 
+
+showInfo :: IO ()
+showInfo = print "Bark v0.1.0. commands available:\n\
+  \build: build the current project\n\
+  \watch: watch the directory and build whenever changes are detected."
+
+execCommand :: String -> IO ()
+execCommand = undefined
+
 main :: IO ()
 main = do
+  args <- getArgs
+  when (null args) $ do
+    showInfo
+    exitSuccess
+
   watchProject "scratch"
